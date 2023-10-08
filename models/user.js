b685b298b7b64f4fb225fa07a6c6885bbdb5f73c
PoonamRai23 <raip0140@gmail.com>
2023-10-08 22:57:39 +0530

    getmessagetask8

---
+++ models/user.js
@@ -0,0 +1,30 @@
+const sequelize=require('sequelize')
+const Sequelize=require('../util/chatAPP')
+const user=Sequelize.define('user',{
+    id:{
+        type:sequelize.INTEGER,
+        autoIncrement:true,
+        allowNull:false,
+        primaryKey:true,
+        unique:true
+    },
+    name:{
+       type:sequelize.STRING,
+    },
+    email:{
+        type:sequelize.STRING,
+        allowNull:false,
+         unique:true
+    },
+    password:{
+        type:sequelize.STRING,
+        allowNull:false
+
+    },
+    number:{
+        type:sequelize.INTEGER,
+        allowNull:false
+   
+},
+})
+module.exports=user
